4fa37374f5123146aeccbc6152dcbb43b22c1a48
jessicaespindola <39319821+jessicaespindola@users.noreply.github.com>
2018-12-01 20:50:20 -0200

    Adicionado os utils, index.js e packages

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const database = require('./config/database');
+const app = require('./config/express');
+
+database(process.env.MONGODB_URI || 'mongodb://localhost/representar');
+require('./models');
+
+const resources = require('./resources');
+resources(app);
+
+const port = process.env.PORT || 3000;
+http.createServer(app).listen(port, () => {
+    console.log('Servidor iniciado na porta', port);
+});
